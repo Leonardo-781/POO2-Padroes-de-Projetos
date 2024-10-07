e81074334d07ca1ccbe3ce732058846c7d7cb44f
Leonardo Cardoso <107780220+Leonardo-781@users.noreply.github.com>
2024-10-07 00:03:42 -0300

    Add files via upload

---
+++ Lab04/Lab04-Atv02/src/main/java/com/mycompany/lab04/atv02/Lab04Atv02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab04.atv02;
+
+/**
+ *
+ * @author leona
+ */
+public class Lab04Atv02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
