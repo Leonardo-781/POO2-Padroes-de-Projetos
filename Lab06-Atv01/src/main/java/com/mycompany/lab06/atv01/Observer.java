765088cec02c3080cb5c73ff81c7e6f849b5fa4c
Leonardo Cardoso <107780220+Leonardo-781@users.noreply.github.com>
2024-11-11 11:55:00 -0300

    Add files via upload

---
+++ Lab06-Atv01/src/main/java/com/mycompany/lab06/atv01/Observer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.lab06.atv01;
+
+/**
+ *
+ * @author leona
+ */
+public interface Observer {
+    void update(float data);
+}
